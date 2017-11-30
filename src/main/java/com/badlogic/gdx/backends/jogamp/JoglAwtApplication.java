74ae5f862445934f6c26711784991ab0979b2d34
Yuri Pourre <yuripourre@gmail.com>
2017-11-30 11:09:16 -0200

    Fix fullscreen and close

--- src/main/java/com/badlogic/gdx/backends/jogamp/JoglAwtApplication.java
+++ src/main/java/com/badlogic/gdx/backends/jogamp/JoglAwtApplication.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2015 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,6 @@
  ******************************************************************************/
 package com.badlogic.gdx.backends.jogamp;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Frame;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.jogamp.JoglAwtGraphics.JoglAwtDisplayMode;
@@ -31,143 +22,175 @@ import com.badlogic.gdx.utils.Clipboard;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.jogamp.opengl.awt.GLCanvas;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class JoglAwtApplication extends JoglApplicationBase {
 
-	Frame frame;
-	
-	/** Creates a new {@link JoglAwtApplication} with the given title and dimensions.
-	 * 
-	 * @param listener the ApplicationListener implementing the program logic
-	 * @param title the title of the application
-	 * @param width the width of the surface in pixels
-	 * @param height the height of the surface in pixels*/
-	public JoglAwtApplication (final ApplicationListener listener, final String title, final int width, final int height) {
-		this(listener, new JoglAwtApplicationConfiguration(title, width, height));
-	}
+    Frame frame;
 
-	public JoglAwtApplication (final ApplicationListener listener, final JoglAwtApplicationConfiguration config) {
-		super(listener, config);
-		initialize(listener, config);
-	}
+    /**
+     * Creates a new {@link JoglAwtApplication} with the given title and dimensions.
+     *
+     * @param listener the ApplicationListener implementing the program logic
+     * @param title    the title of the application
+     * @param width    the width of the surface in pixels
+     * @param height   the height of the surface in pixels
+     */
+    public JoglAwtApplication(final ApplicationListener listener, final String title, final int width, final int height) {
+        this(listener, new JoglAwtApplicationConfiguration(title, width, height));
+    }
 
-	protected void initialize(final ApplicationListener listener, final JoglAwtApplicationConfiguration config) {
-		final Runnable runnable = new Runnable() {
+    public JoglAwtApplication(final ApplicationListener listener, final JoglAwtApplicationConfiguration config) {
+        super(listener, config);
+        initialize(listener, config);
+    }
 
-			@Override
-			public void run() {
-				if (!config.fullscreen) {
-					frame = new Frame(config.title);
-					((JoglAwtGraphics)graphics).getCanvas().setPreferredSize(new Dimension(config.width, config.height));
-					frame.setSize(config.width + frame.getInsets().left + frame.getInsets().right, frame.getInsets().top
-							+ frame.getInsets().bottom + config.height);
-					frame.add(((JoglAwtGraphics)graphics).getCanvas(), BorderLayout.CENTER);
-					frame.setLocationRelativeTo(null);
-					frame.addWindowListener(windowListener);
+    protected void initialize(final ApplicationListener listener, final JoglAwtApplicationConfiguration config) {
+        final Runnable runnable = new Runnable() {
 
-					frame.pack();
-					frame.setVisible(true);
-				} else {
-					GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
-					GraphicsDevice device = genv.getDefaultScreenDevice();
-					frame = new Frame(config.title);
-					((JoglAwtGraphics)graphics).getCanvas().setPreferredSize(new Dimension(config.width, config.height));
-					frame.setSize(config.width + frame.getInsets().left + frame.getInsets().right, frame.getInsets().top
-							+ frame.getInsets().bottom + config.height);
-					frame.add(((JoglAwtGraphics)graphics).getCanvas(), BorderLayout.CENTER);
-					frame.setLocationRelativeTo(null);
-					frame.addWindowListener(windowListener);
-					frame.setUndecorated(true);
-					frame.setResizable(false);
-					frame.pack();
-					frame.setVisible(true);
-					java.awt.DisplayMode desktopMode = device.getDisplayMode();
-					try {
-						device.setFullScreenWindow(frame);
-						JoglAwtDisplayMode mode = ((JoglAwtGraphics)graphics).findBestMatch(config.width, config.height);
-						if (mode == null)
-							throw new GdxRuntimeException("Couldn't set fullscreen mode " + config.width + "x" + config.height);
-						device.setDisplayMode(mode.mode);
-					} catch (Throwable e) {
-						e.printStackTrace();
-						device.setDisplayMode(desktopMode);
-						device.setFullScreenWindow(null);
-						frame.dispose();
-						audio.dispose();
-						System.exit(-1);
-					}
-				}
-			}
-		};
-		if (EventQueue.isDispatchThread()) {
-			runnable.run();
-		} else {
-			try {
-				EventQueue.invokeAndWait(runnable);
-			} catch (Throwable t) {
-				throw new GdxRuntimeException("Creating window failed", t);
-			}
-		}
-	}
-	
-	final WindowAdapter windowListener = new WindowAdapter() {
-		@Override
-		public void windowOpened (WindowEvent arg0) {
-			((JoglAwtGraphics)graphics).getCanvas().requestFocus();
-			((JoglAwtGraphics)graphics).getCanvas().requestFocusInWindow();
-		}
+            @Override
+            public void run() {
+                if (!config.fullscreen) {
+                    frame = new Frame(config.title);
+                    ((JoglAwtGraphics) graphics).getCanvas().setPreferredSize(new Dimension(config.width, config.height));
+                    frame.setSize(config.width + frame.getInsets().left + frame.getInsets().right, frame.getInsets().top
+                            + frame.getInsets().bottom + config.height);
+                    frame.add(((JoglAwtGraphics) graphics).getCanvas(), BorderLayout.CENTER);
+                    frame.setLocationRelativeTo(null);
+                    frame.addWindowListener(windowListener);
 
-		@Override
-		public void windowIconified (WindowEvent arg0) {
-		}
+                    frame.pack();
+                    frame.setVisible(true);
+                } else {
+                    frame = new Frame(config.title);
+                    ((JoglAwtGraphics) graphics).getCanvas().setPreferredSize(new Dimension(config.width, config.height));
+                    frame.setSize(config.width + frame.getInsets().left + frame.getInsets().right, frame.getInsets().top
+                            + frame.getInsets().bottom + config.height);
+                    frame.add(((JoglAwtGraphics) graphics).getCanvas(), BorderLayout.CENTER);
+                    frame.setLocationRelativeTo(null);
+                    frame.addWindowListener(windowListener);
+                    frame.setUndecorated(true);
+                    frame.setResizable(true);
+                    frame.setAlwaysOnTop(true);
+                    frame.pack();
+                    frame.setVisible(true);
 
-		@Override
-		public void windowDeiconified (WindowEvent arg0) {
-		}
+                    GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                    GraphicsDevice device = getScreen(frame, genv);
 
-		@Override
-		public void windowClosing (WindowEvent arg0) {
-			graphics.setContinuousRendering(true);
-			graphics.pause();
-			graphics.destroy();
-			audio.dispose();
-			frame.remove(((JoglAwtGraphics)graphics).getCanvas());
-			frame.dispose();
-		}
-	};
-	
-	/** @return the drawable of the application. */
-	@Override
-	public GLCanvas getGLCanvas() {
-		return ((JoglAwtGraphics)graphics).getCanvas();
-	}
-	
-	/** @return the Frame of the application. */
-	public Frame getFrame () {
-		return frame;
-	}
+                    java.awt.DisplayMode desktopMode = device.getDisplayMode();
+                    try {
+                        device.setFullScreenWindow(frame);
+                        JoglAwtDisplayMode mode = ((JoglAwtGraphics) graphics).findBestMatch(config.width, config.height);
+                        if (mode == null)
+                            throw new GdxRuntimeException("Couldn't set fullscreen mode " + config.width + "x" + config.height);
+                        device.setDisplayMode(mode.mode);
 
-	@Override
-	public Clipboard getClipboard() {
-		return new JoglAwtClipboard();
-	}
+                        Rectangle bounds = device.getDefaultConfiguration().getBounds();
+                        frame.setBounds(bounds.x, bounds.y, bounds.width, bounds.height);
 
-	@Override
-	protected JoglAwtGraphics createGraphics(ApplicationListener listener, JoglApplicationConfiguration config) {
-		return new JoglAwtGraphics(listener, (JoglAwtApplicationConfiguration) config);
-	}
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                        device.setDisplayMode(desktopMode);
+                        device.setFullScreenWindow(null);
+                        frame.dispose();
+                        audio.dispose();
+                        System.exit(-1);
+                    }
+                }
+            }
+        };
+        if (EventQueue.isDispatchThread()) {
+            runnable.run();
+        } else {
+            try {
+                EventQueue.invokeAndWait(runnable);
+            } catch (Throwable t) {
+                throw new GdxRuntimeException("Creating window failed", t);
+            }
+        }
+    }
 
-	@Override
-	protected Input createInput(JoglGraphicsBase graphics) {
-		return new JoglAwtInput(((JoglAwtGraphics)graphics).getCanvas());
-	}
-	
-	@Override
-	public void exit () {
-		postRunnable(new Runnable() {
-			@Override
-			public void run () {
-				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-			}
-		});
-	}
+    private GraphicsDevice getScreen(Frame frame, GraphicsEnvironment genv) {
+        Point point = frame.getLocation();
+
+        GraphicsDevice[] allScreens = genv.getScreenDevices();
+        for (int i = 0; i < allScreens.length; i++) {
+            Rectangle bounds = allScreens[i].getDefaultConfiguration().getBounds();
+            if (bounds.x < point.x && bounds.x + bounds.width > point.x) {
+                return allScreens[i];
+            }
+        }
+        return allScreens[0];
+    }
+
+    final WindowAdapter windowListener = new WindowAdapter() {
+        @Override
+        public void windowOpened(WindowEvent arg0) {
+            ((JoglAwtGraphics) graphics).getCanvas().requestFocus();
+            ((JoglAwtGraphics) graphics).getCanvas().requestFocusInWindow();
+        }
+
+        @Override
+        public void windowIconified(WindowEvent arg0) {
+        }
+
+        @Override
+        public void windowDeiconified(WindowEvent arg0) {
+        }
+
+        @Override
+        public void windowClosing(WindowEvent arg0) {
+            graphics.setContinuousRendering(true);
+            graphics.pause();
+            graphics.destroy();
+            audio.dispose();
+            frame.setVisible(false);
+            frame.remove(((JoglAwtGraphics) graphics).getCanvas());
+            frame.dispose();
+        }
+    };
+
+    /**
+     * @return the drawable of the application.
+     */
+    @Override
+    public GLCanvas getGLCanvas() {
+        return ((JoglAwtGraphics) graphics).getCanvas();
+    }
+
+    /**
+     * @return the Frame of the application.
+     */
+    public Frame getFrame() {
+        return frame;
+    }
+
+    @Override
+    public Clipboard getClipboard() {
+        return new JoglAwtClipboard();
+    }
+
+    @Override
+    protected JoglAwtGraphics createGraphics(ApplicationListener listener, JoglApplicationConfiguration config) {
+        return new JoglAwtGraphics(listener, (JoglAwtApplicationConfiguration) config);
+    }
+
+    @Override
+    protected Input createInput(JoglGraphicsBase graphics) {
+        return new JoglAwtInput(((JoglAwtGraphics) graphics).getCanvas());
+    }
+
+    @Override
+    public void exit() {
+        postRunnable(new Runnable() {
+            @Override
+            public void run() {
+                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+                JoglAwtApplication.super.exit();
+            }
+        });
+    }
 }
